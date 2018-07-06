e47671a0ec5ef12c7440f898fbc4eec08d4279d6
Ben Davis <bendavis381@gmail.com>
2018-07-06 01:06:02 -0400

    Pricing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,40 @@ module.exports = {
         path: `${__dirname}/content/pages/home`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages/pricing`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-apiserver',
+      options: {
+        // Type prefix of entities from server
+        typePrefix: 'Gather',
+        // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
+        // using this name. i.e. posts.json
+        name: `Services`,
+
+        // The url, this should be the endpoint you are attempting to pull data from
+        url: `https://api.gatherdata.co/services`,
+
+        method: 'get',
+
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        // Nested level of entities in repsonse object, example: `data.posts`
+        entityLevel: `services`,
+
+        // Simple authentication, if optional, set it null
+        auth: null,
+
+        // Optional payload key name if your api returns your payload in a different key
+        // Default will use the full response from the http request of the url
+        // payloadKey: `body`,
+      },
+    },
   ],
 };
---
+++ src/pages/pricing.tsx
@@ -0,0 +1,239 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  Text,
+  TextTypes,
+  Link,
+  LinkTypes,
+  Flex,
+  pv,
+  maxWidth,
+  Column,
+  Page,
+  pageSmallWidth,
+  Row,
+  p,
+  ph,
+  mv,
+  mb,
+  borderRadius,
+  colors,
+  border,
+  mt,
+} from 'gather-style';
+import IOSCheckmarkOutline from 'react-icons/lib/io/ios-checkmark-outline';
+
+import FooterCta from '../components/FooterCta';
+
+const TitleContainer = styled(Column)`
+  ${pv(12)};
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+`;
+
+const Title = styled(Text)`
+  ${maxWidth(60)};
+`;
+
+const PricingBoxes = styled(Flex)`
+  justify-content: stretch;
+  width: 100%;
+`;
+
+const StandardBox = styled.div`
+  ${borderRadius};
+  background: ${colors.purple};
+  ${pv(3)};
+  ${ph(4)};
+  flex: 1;
+`;
+
+const EnterpriseBox = styled.div`
+  ${borderRadius};
+  ${border};
+  ${pv(3)};
+  ${ph(4)};
+  flex: 1;
+`;
+
+const Divider = styled.div`
+  margin: 0 -2rem;
+  background: ${props => props.color || colors.white20};
+  height: 1px;
+  ${mv(3)};
+`;
+
+const Benefit = styled.div`
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+
+  &:not(:last-child) {
+    ${mb(2)};
+  }
+`;
+
+const FAQs = styled(Flex)`
+  ${mt(30)};
+  ${mb(10)};
+`;
+
+const Questions = styled(Row)`
+  ${mt(8)};
+`;
+
+const Pricing = ({ data: { pricingJson: pricing, footerJson: footer } }) => {
+  return (
+    <div>
+      <Page width={pageSmallWidth}>
+        <Row>
+          <TitleContainer>
+            <Title align="center" type={TextTypes.HEADING_1}>
+              {pricing.title}
+            </Title>
+          </TitleContainer>
+        </Row>
+        <Row>
+          <Column>
+            <PricingBoxes>
+              <StandardBox>
+                <Text
+                  color={colors.white}
+                  type={TextTypes.HEADING_3}
+                  heavy={false}
+                >
+                  Standard
+                </Text>
+                <Divider />
+                <Flex alignItems="baseline">
+                  <Text color={colors.white} type={TextTypes.HEADING_1}>
+                    {`$${pricing.standard.pricePerMonth}`}
+                  </Text>
+                  <Text
+                    color={colors.white}
+                    ml={2}
+                    mb={3}
+                    type={TextTypes.BODY}
+                  >
+                    per user / month
+                  </Text>
+                </Flex>
+                <Link
+                  type={LinkTypes.BUTTON_PRIMARY}
+                  textColor={colors.purple}
+                  color={colors.white}
+                  href="https://app.gatherdata.co/signup"
+                  mb={3}
+                >
+                  {pricing.standard.cta}
+                </Link>
+                <Flex alignItems="flex-start" flow="column">
+                  {pricing.standard.benefits.map((benefit, index) => (
+                    <Benefit>
+                      <IOSCheckmarkOutline size={24} color={colors.blue} />
+                      <Text color={colors.white} ml={1}>
+                        {benefit}
+                      </Text>
+                    </Benefit>
+                  ))}
+                </Flex>
+              </StandardBox>
+              <EnterpriseBox>
+                <Text type={TextTypes.HEADING_3} heavy={false}>
+                  Enterprise
+                </Text>
+                <Divider color={colors.border} />
+                <Flex alignItems="flex-start" flow="column">
+                  {pricing.enterpise.benefits.map(benefit => (
+                    <Benefit>
+                      <IOSCheckmarkOutline size={24} color={colors.blue} />
+                      <Text ml={1}>{benefit}</Text>
+                    </Benefit>
+                  ))}
+                </Flex>
+                <Link
+                  mt={3}
+                  type={LinkTypes.BUTTON_DEFAULT}
+                  href="https://app.gatherdata.co/signup"
+                >
+                  {pricing.enterpise.cta}
+                </Link>
+              </EnterpriseBox>
+            </PricingBoxes>
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <FAQs flow="column">
+              <Text type={TextTypes.HEADING_2}>Frequently Asked Questions</Text>
+              <Flex>
+                <Text>Have another question?</Text>
+                <Link href="mailto:support@gatherdata.co" heavy ml={1}>
+                  Let's talk
+                </Link>
+              </Flex>
+              <Questions>
+                {pricing.faqs.map(faq => (
+                  <Column md={6}>
+                    <Text heavy mb={1}>
+                      {faq.question}
+                    </Text>
+                    <Text color={colors.text}>{faq.answer}</Text>
+                  </Column>
+                ))}
+              </Questions>
+            </FAQs>
+          </Column>
+        </Row>
+      </Page>
+      <FooterCta
+        title={pricing.footerCta.title}
+        subtitle={pricing.footerCta.subtitle}
+        ctaText={pricing.footerCta.ctaText}
+        copyright={footer.copyright}
+        madeIn={footer.madeIn}
+        linkGroups={footer.linkGroups}
+      />
+    </div>
+  );
+};
+
+export default Pricing;
+
+export const query = graphql`
+  query PricingQuery {
+    footerJson {
+      copyright
+      madeIn
+      linkGroups {
+        title
+        links {
+          label
+          to
+        }
+      }
+    }
+    pricingJson {
+      title
+      standard {
+        pricePerMonth
+        cta
+        benefits
+      }
+      enterpise {
+        cta
+        benefits
+      }
+      faqs {
+        question
+        answer
+      }
+      footerCta {
+        title
+        subtitle
+        ctaText
+      }
+    }
+  }
+`;
