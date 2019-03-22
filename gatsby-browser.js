3ee40bc16d610d8c1ad4ca0576520ea2a96286bd
Ben Davis <bendavis381@gmail.com>
2019-03-22 00:11:08 -0700

    Force page reloads to fix SSR bug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,11 @@ require('prismjs');
 require('./src/prism-theme.css');
 
 exports.disableCorePrefetching = () => true;
+exports.onClientEntry = () => {
+  var originalPushState = window.history.pushState;
+
+  window.history.pushState = function(a, b, c) {
+    originalPushState(a, b, c);
+    window.location.reload();
+  };
+};
